34583574c02130441b3a906ac3c36d4a84111971
Doanta Ginting <doantaaloycius@gmail.com>
2025-04-18 01:27:03 +0700

    Change typing speed to guess word

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import subprocess
 import sys
 
 
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def run_game(path: str):
     """
     Menjalankan file python game berdasarkan path.
@@ -42,12 +47,29 @@ def main():
     actions = {
         0: lambda: run_game("tic_tac_toe/tic_tac_toe_main.py"),
         1: lambda: run_game("rpg/rpg_main.py"),
-        2: lambda: run_game("typing_speed/typing_speed_main.py"),
+        2: lambda: run_game("guess_words/guess_words_main.py"),
         3: exit_program
     }
 
     while True:
-        terminal_menu = TerminalMenu(menu_options, title="🎮 Main Menu")
+        clear_screen()
+        title = """
+  __  __ _       _    _____                      
+ |  \/  (_)     (_)  / ____|                     
+ | \  / |_ _ __  _  | |  __  __ _ _ __ ___   ___ 
+ | |\/| | | '_ \| | | | |_ |/ _` | '_ ` _ \ / _ |
+ | |  | | | | | | | | |__| | (_| | | | | | |  __/
+ |_|  |_|_|_| |_|_|  \_____|\__,_|_| |_| |_|\___|
+  _______                     _  _               
+ |__   __|                   | || |              
+    | | ___  __ _ _ __ ___   | || |_             
+    | |/ _ \/ _` | '_ ` _ \  |__   _|            
+    | |  __/ (_| | | | | | |    | |              
+    |_|\___|\__,_|_| |_| |_|    |_|              
+                                                 
+                                                 
+        """
+        terminal_menu = TerminalMenu(menu_options, title=title)
         selected_index = terminal_menu.show()
 
         action = actions.get(selected_index)
